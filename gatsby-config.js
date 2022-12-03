74d4bff0d28861157d171a03a177c0b9e58c8b70
Aftab69 <aftabhssn3@gmail.com>
2022-12-03 23:30:09 +0530

    metadata changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Aftab Hussain Portfolio`,
+    siteTitleAlt: `Aftab Hussain Portfolio`,
+    siteHeadline: `Aftab Hussain Portfolio`,
+    siteUrl: `https://aftabhussaingatsbyportfolio.netlify.app/`,
+    siteDescription: `Portfolio by Aftab Hussain`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `Aftab Hussain`,
   },
   trailingSlash: `never`,
   plugins: [
